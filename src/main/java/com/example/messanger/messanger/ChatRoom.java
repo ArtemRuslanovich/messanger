778a311efdb46b1433aa15af5aafc8b70d4bc6c4
Admin <artem-ruslanovich@inbox.ru>
2024-03-03 21:53:12 +0300

    add chatroom class

---
+++ src/main/java/com/example/messanger/messanger/ChatRoom.java
@@ -0,0 +1,44 @@
+package com.example.messanger.messanger;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class ChatRoom {
+    private String id;
+    private String name;
+    private Set<User> users = new HashSet<>();
+
+    public ChatRoom(String name) {
+        this.id = UUID.randomUUID().toString(); // Генерация уникального идентификатора для комнаты
+        this.name = name;
+    }
+
+    // Добавление пользователя в чат-комнату
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+
+    // Удаление пользователя из чат-комнаты
+    public void removeUser(User user) {
+        this.users.remove(user);
+    }
+
+    // Геттеры и сеттеры
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return Collections.unmodifiableSet(users);
+    }
+}
--- src/main/java/com/example/messanger/messanger/MessageController.java
+++ src/main/java/com/example/messanger/messanger/MessageController.java
@@ -1,16 +1,25 @@
 package com.example.messanger.messanger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+//import com.example.messanger.messanger.Message;
 
 @Controller
 public class MessageController {
-    
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    public MessageController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
     @MessageMapping("/chat")
-    @SendTo("/topic/messages")
-    public Message handleChatMessage(@RequestBody Message message) {
-        return message;
+    public void handleChatMessage(Message message) {
+        // Используем SimpMessagingTemplate для отправки сообщения конкретному получателю
+        messagingTemplate.convertAndSendToUser(
+            message.getTo(), "/queue/messages", message);
     }
 }
